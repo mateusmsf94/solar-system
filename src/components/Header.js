f78dd1bab7c6a7e545fe1ff02af19e252f23a8ac
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-04-24 13:09:59 -0300

    commitao

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className='flex items-center justify-center'>
+        <h1 className='text-3xl font-bold text-slate-50 mt-5'>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
